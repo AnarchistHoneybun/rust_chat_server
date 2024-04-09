536971b34bf6aa6ca833f8b74b82b78e0ee4e357
AnarchistHoneybun <thatonebipanda@gmail.com>
2024-04-09 18:05:02 +0530

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use tokio::{io::{AsyncReadExt, AsyncWriteExt, BufReader}, net::TcpListener};
+use tokio::io::AsyncBufReadExt;
+
+#[tokio::main]
+async fn main() {
+    let listener: TcpListener = TcpListener::bind("localhost:8080").await.unwrap();
+
+    let (mut socket, _addr) = listener.accept().await.unwrap();
+
+    let (read_half, mut write_half) = socket.split();
+
+    let mut reader = BufReader::new(read_half);
+    let mut line = String::new();
+
+    loop {
+
+        let bytes_read = reader.read_line(&mut line).await.unwrap();
+
+        if bytes_read == 0 {
+            break;
+        }
+
+        write_half.write_all(line.as_bytes()).await.unwrap();
+        line.clear();
+    }
+
+}
