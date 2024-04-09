156c43e1c90be31ca8a569df69cdbf95b755786d
AnarchistHoneybun <thatonebipanda@gmail.com>
2024-04-09 22:47:30 +0530

    fix self response

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,10 @@ use tokio::sync::broadcast;
 async fn main() {
     let listener: TcpListener = TcpListener::bind("localhost:8080").await.unwrap();
 
-    let (tx, _rx) = broadcast::channel::<String>(10);
+    let (tx, _rx) = broadcast::channel(10);
 
     loop {
-        let (mut socket, _addr) = listener.accept().await.unwrap();
+        let (mut socket, addr) = listener.accept().await.unwrap();
 
         let tx = tx.clone();
         let mut rx = tx.subscribe();
@@ -27,12 +27,15 @@ async fn main() {
                             break;
                         }
 
-                        tx.send(line.clone()).unwrap();
+                        tx.send((line.clone(), addr)).unwrap();
                         line.clear();
                     },
                     result = rx.recv() => {
-                        let msg = result.unwrap();
+                        let (msg, other_addr ) = result.unwrap();
+
+                        if addr!= other_addr {
                         write_half.write_all(msg.as_bytes()).await.unwrap();
+                            }
                     }
                 }
 
