fbfa432ceeb21b869677dc5781e6f6fc99004b6b
AnarchistHoneybun <thatonebipanda@gmail.com>
2024-04-09 22:37:14 +0530

    broadcast working properly now

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 use tokio::{io::{AsyncReadExt, AsyncWriteExt, BufReader}, net::TcpListener};
 use tokio::io::AsyncBufReadExt;
+use tokio::sync::broadcast;
 
 #[tokio::main]
 async fn main() {
     let listener: TcpListener = TcpListener::bind("localhost:8080").await.unwrap();
 
+    let (tx, _rx) = broadcast::channel::<String>(10);
+
     loop {
         let (mut socket, _addr) = listener.accept().await.unwrap();
+
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
 
         tokio::spawn(async move{
             let (read_half, mut write_half) = socket.split();
@@ -15,14 +21,21 @@ async fn main() {
             let mut line = String::new();
 
             loop {
-                let bytes_read = reader.read_line(&mut line).await.unwrap();
+                tokio::select! {
+                    result = reader.read_line(&mut line) => {
+                        if result.unwrap() == 0 {
+                            break;
+                        }
 
-                if bytes_read == 0 {
-                    break;
+                        tx.send(line.clone()).unwrap();
+                        line.clear();
+                    },
+                    result = rx.recv() => {
+                        let msg = result.unwrap();
+                        write_half.write_all(msg.as_bytes()).await.unwrap();
+                    }
                 }
 
-                write_half.write_all(line.as_bytes()).await.unwrap();
-                line.clear();
             }
         });
     }
