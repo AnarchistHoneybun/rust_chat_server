04e367cb189d6c31814566efad0be58e27df8d19
AnarchistHoneybun <thatonebipanda@gmail.com>
2024-04-25 15:20:31 +0530

    colors for help implemented properly

--- src/client_commands.rs
+++ src/client_commands.rs
@@ -259,74 +259,71 @@ pub(crate) async fn handle_help_command(write_half: &mut WriteHalf<'_>, line: &s
     if let Some(command) = parts.next() {
         match command {
             "/create_room" => {
-                write_half.write_all(b"\n/create_room <room_name> - Create a new chat room.\nUse an underscore between multi-word room names.\nRoom names 'glb' and 'adm' are reserved.\n\n")
+                write_half.write_all(format!("{}\n/create_room <room_name> - Create a new chat room.\nUse an underscore between multi-word room names.\nRoom names 'glb' and 'adm' are reserved.\n{}\n", color_codes::YELLOW,color_codes::RESET).as_bytes())
                     .await
                     .unwrap();
             }
             "/join_room" => {
-                write_half.write_all(b"\n/join_room <room_name> - Join an existing chat room.\nYou must provide a valid room name.\nUse '/view_rooms' to list available rooms.\n\n")
+                write_half.write_all(format!("{}\n/join_room <room_name> - Join an existing chat room.\nYou must provide a valid room name.\nUse '/view_rooms' to list available rooms.\n{}\n", color_codes::YELLOW,color_codes::RESET).as_bytes())
                     .await
                     .unwrap();
             }
             "/view_rooms" => {
-                write_half
-                    .write_all(b"\n/view_rooms - View all chat rooms.\n\n")
+                write_half.write_all(format!("{}\n/view_rooms - View all chat rooms.\n{}\n", color_codes::YELLOW,color_codes::RESET).as_bytes())
                     .await
                     .unwrap();
             }
             "/leave_room" => {
-                write_half.write_all(b"\n/leave_room <room_name> - Leave a chat room.\nYou must be a member of the room to leave it.\n\n")
+                write_half.write_all(format!("{}\n/leave_room <room_name> - Leave a chat room.\nYou must be a member of the room to leave it.\n{}\n", color_codes::YELLOW,color_codes::RESET).as_bytes())
                     .await
                     .unwrap();
             }
             "/m_room" => {
-                write_half.write_all(b"\n/m_room <room_name> <message> - Send a message to all users in a specific room.\nYou must be a member of the room to send a message.\n\n")
+                write_half.write_all(format!("{}\n/m_room <room_name> <message> - Send a message to all users in a specific room.\nYou must be a member of the room to send a message.\n{}\n", color_codes::YELLOW,color_codes::RESET).as_bytes())
                     .await
                     .unwrap();
             }
             "/view_users" => {
-                write_half.write_all(b"\n/view_users <room_name> - View users in a specific chat room.\nYou must be a member of the room to view its users.\n\n")
+                write_half.write_all(format!("{}\n/view_users <room_name> - View users in a specific chat room.\nYou must be a member of the room to view its users.\n{}\n", color_codes::YELLOW,color_codes::RESET).as_bytes())
                     .await
                     .unwrap();
             }
             "/list" => {
-                write_half
-                    .write_all(b"\n/list - List all connected users.\n\n")
+                write_half.write_all(format!("{}\n/list - List all connected users.\n{}\n", color_codes::YELLOW,color_codes::RESET).as_bytes())
                     .await
                     .unwrap();
             }
             "/report" => {
-                write_half.write_all(b"\n/report <username> - Report a user to the server admin.\nYou must provide a valid username.\n\n")
+                write_half.write_all(format!("{}\n/report <username> - Report a user to the server admin.\nYou must provide a valid username.\n{}\n", color_codes::YELLOW,color_codes::RESET).as_bytes())
                     .await
                     .unwrap();
             }
             "/pm" => {
-                write_half.write_all(b"\n/pm <username> <message> - Send a private message to any connected user.\nYou must provide a valid username and a message.\n\n")
+                write_half.write_all(format!("{}\n/pm <username> <message> - Send a private message to any connected user.\nYou must provide a valid username and a message.\n{}\n", color_codes::YELLOW,color_codes::RESET).as_bytes())
                     .await
                     .unwrap();
             }
             "/exit" => {
-                write_half.write_all(format!("{}\n/exit - Disconnect from the server{}\n\n", color_codes::YELLOW, color_codes::RESET).as_bytes())
+                write_half.write_all(format!("{}\n/exit - Disconnect from the server{}\n\n", color_codes::YELLOW,color_codes::RESET).as_bytes())
                     .await
                     .unwrap();
             }
             _ => {
-                write_half.write_all(format!("\n{}Command not found{}\n\n", color_codes::RED, color_codes::RESET).as_bytes()).await.unwrap();
+                write_half.write_all(format!("{}\nCommand not found{}\n\n", color_codes::RED, color_codes::RESET).as_bytes()).await.unwrap();
             }
         }
     } else {
-        let help_text = "\nuse /help <command> to get details on a specific command\n
-        
-/list        - List all connected users
-/pm          - Send a private message to any connected user
-/report      - Report a user to the server admin
-/exit        - Disconnect from the server
-/create_room - Create a new chat room
-/join_room   - Join an existing chat room
-/leave_room  - Leave a chat room
-/view_rooms  - View all chat rooms
-/view_users  - View users in a specific chat room
-/m_room      - Send a message to all users in a specific room\n";
+        let help_text = format!("{}use /help <command> to get details on a specific command{}\n
+        {}/list        - List all connected users
+        /pm          - Send a private message to any connected user
+        /report      - Report a user to the server admin
+        /exit        - Disconnect from the server
+        /create_room - Create a new chat room
+        /join_room   - Join an existing chat room
+        /leave_room  - Leave a chat room
+        /view_rooms  - View all chat rooms
+        /view_users  - View users in a specific chat room
+        /m_room      - Send a message to all users in a specific room{}\n\n", color_codes::GREEN, color_codes::RESET, color_codes::YELLOW, color_codes::RESET);
 
         write_half.write_all(help_text.as_bytes()).await.unwrap();
     }
